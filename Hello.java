2f6872b11b72d206dd9243f5472b681859c17e4b
LuzAndreaASW <lmarino@asesoftware.com>
2018-10-04 12:13:45 -0500

    Ejemplo Jenkins

--- Hello.java
+++ Hello.java
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 public class Hello{
 	public static void main(String[] args){
 		for(int i = 0; i<= 10; i++){
 			System.out.println("Hola mundo..." + i);
 		}
 	}
+=======
+public class Hello{
+	public static void main(String[] args){
+		for(int i = 0; i<= 10; i++){
+			System.out.println("Hola mundo..." + i);
+		}
+	}
+>>>>>>> Ejemplo Jenkins
 }
